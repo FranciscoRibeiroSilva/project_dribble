34b8e5954d069679b31e64e24843ce8da2c55990
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2023-06-30 16:28:25 -0300

    Last

---
+++ lib/screens/helper_centre.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HelperCentre extends StatelessWidget {
+  const HelperCentre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
